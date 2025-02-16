3b08de95b385b62c4e98af8eff14cc5fb111a7b1
Romina Esparza <romi.anahi01@outlook.com>
2025-02-15 20:43:08 -0500

    fixed fetching a single recipe

--- src/pages/SingleRecipeSearchPage/SingleRecipeSearchPage.jsx
+++ src/pages/SingleRecipeSearchPage/SingleRecipeSearchPage.jsx
@@ -6,12 +6,11 @@ import { useParams } from "react-router-dom";
 export default function SingleRecipeSearchPage() {
     const [recipe, setRecipe] = useState(null); // Store single recipe data
     const { recipeId } = useParams();
-    console.log("this is the id", recipeId);
 
     const getRecipeById = async (id) => {
         try {
             const response = await axios.get(`https://dummyjson.com/recipes/${id}`);
-            console.log('hello');
+            console.log(response.data);
             setRecipe(response.data); // Set the recipe data directly
         } catch (error) {
             console.error("Error fetching recipe", error);
@@ -19,7 +18,6 @@ export default function SingleRecipeSearchPage() {
     };
 
     useEffect(() => {
-      console.log("useEffect triggered, recipeId:", recipeId); // Debugging
         if (recipeId !== undefined) {
             getRecipeById(recipeId);
         }
@@ -27,7 +25,9 @@ export default function SingleRecipeSearchPage() {
 
     return (
         <section className="single-recipe">
-          {recipe.name}
+          {recipe && (
+            <p>{recipe.name}</p>
+          )}
         </section>
     );
 }
