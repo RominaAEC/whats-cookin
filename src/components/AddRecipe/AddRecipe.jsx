730a9df8ca1ecccc730342ba553150700d41239c
Romina Esparza <romi.anahi01@outlook.com>
2025-02-16 16:59:04 -0500

    fixed navigation hidden on certain pages

---
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -0,0 +1,10 @@
+import "./AddRecipe.scss";
+
+export default function AddRecipe() {
+  return (
+    <div>
+      this is the form 
+    </div>
+  )
+}
+
--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,9 +1,11 @@
 import "./AddRecipePage.scss";
+import AddRecipe from "../../components/AddRecipe/AddRecipe";
 
 export default function AddRecipePage() {
   return (
     <div>
       Add Recipe Page
+      <AddRecipe/>
     </div>
   )
 }
