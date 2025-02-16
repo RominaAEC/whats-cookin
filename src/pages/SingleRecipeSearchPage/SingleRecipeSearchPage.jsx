33a664a0ccdb701d1c846c16656e4a4eaaba89e2
Romina Esparza <romi.anahi01@outlook.com>
2025-02-16 11:43:19 -0500

    added complete single recipe card styling

--- src/pages/SingleRecipeSearchPage/SingleRecipeSearchPage.jsx
+++ src/pages/SingleRecipeSearchPage/SingleRecipeSearchPage.jsx
@@ -5,6 +5,9 @@ import { Link, useParams } from "react-router-dom";
 import SearchIcon from "../../assets/icons/search.svg?react";
 import BookIcon from "../../assets/icons/book.svg?react";
 import BackIcon from "../../assets/icons/arrow-back.svg?react";
+import BookmarkIcon from "../../assets/icons/bookmark.svg?react";
+import CarrotIcon from "../../assets/icons/ph_carrot-thin.svg?react";
+import BowlIcon from "../../assets/icons/arcticons_recipe-keeper.svg?react";
 
 export default function SingleRecipeSearchPage() {
     const [recipe, setRecipe] = useState(null); // Store single recipe data
@@ -54,9 +57,53 @@ export default function SingleRecipeSearchPage() {
                         <div className="single-recipe__image-overlay"></div>
                     </div>
                     <div className="single-recipe__card">
-                        <h2>{recipe.name}</h2>
+                        <div className="single-recipe__header">
+                            <div className="single-recipe__title-container">
+                                <h2 className="single-recipe__title">{recipe.name}</h2>
+                            </div>
+                            <button className="single-recipe__header-button">
+                                <BookmarkIcon className="single-recipe__header-icon" />
+                            </button>
+                        </div>
+                        <div className="single-recipe__ingredients-container">
+                            <div className="single-recipe__ingredients">
+                                <div className="single-recipe__ingredients-header">
+                                    <CarrotIcon className="single-recipe__ingredients-icon" />
+                                    <h3 className="single-recipe__ingredients-label">Ingredients</h3>
+                                </div>
+                                <ul className="single-recipe__list">
+                                    {recipe.ingredients.map((ingredient, index) => (
+                                        <li className="single-recipe__list-item" key={index}><p>{ingredient}</p></li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="single-recipe__time-container">
+                                <div className="single-recipe__time">
+                                    <p className="single-recipe__label p1">Prep:</p>
+                                    <p className="single-recipe__detail p1">{recipe.prepTimeMinutes} min</p>
+                                </div>
+                                <div className="single-recipe__time">
+                                    <p className="single-recipe__label p1">Cook:</p>
+                                    <p className="single-recipe__detail p1">{recipe.cookTimeMinutes} min</p>
+                                </div>
+                                <div className="single-recipe__time">
+                                    <p className="single-recipe__label p1">Servings:</p>
+                                    <p className="single-recipe__detail p1">{recipe.servings}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="single-recipe__instructions-container">
+                            <div className="single-recipe__ingredients-header">
+                                <BowlIcon className="single-recipe__ingredients-icon" />
+                                <h3 className="single-recipe__ingredients-label">Instructions</h3>
+                            </div>
+                            <ul className="single-recipe__list">
+                                {recipe.instructions.map((instruction, index) => (
+                                    <li className="single-recipe__list-item" key={index}><p>{instruction}</p></li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                    
                 </article>
             )}
         </section>
