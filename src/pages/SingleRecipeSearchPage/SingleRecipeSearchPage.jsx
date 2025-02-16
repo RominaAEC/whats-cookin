c1f52c10e107088f985e0bd2c334c5f67ebdeadf
Romina Esparza <romi.anahi01@outlook.com>
2025-02-15 22:36:07 -0500

    added single recipe page overlay style

--- src/pages/SingleRecipeSearchPage/SingleRecipeSearchPage.jsx
+++ src/pages/SingleRecipeSearchPage/SingleRecipeSearchPage.jsx
@@ -1,7 +1,10 @@
 import "./SingleRecipeSearchPage.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import SearchIcon from "../../assets/icons/search.svg?react";
+import BookIcon from "../../assets/icons/book.svg?react";
+import BackIcon from "../../assets/icons/arrow-back.svg?react";
 
 export default function SingleRecipeSearchPage() {
     const [recipe, setRecipe] = useState(null); // Store single recipe data
@@ -26,7 +29,32 @@ export default function SingleRecipeSearchPage() {
     return (
         <section className="single-recipe">
           {recipe && (
-            <p>{recipe.name}</p>
+            <article className="single-recipe__container">
+                <div className="single-recipe__navbar">
+                        <Link to="/search-results">
+                                <button className="single-recipe__navbar-button">
+                                    <BackIcon className="single-recipe__navbar-icon" />
+                                </button>
+                            </Link>
+                      <article className="single-recipe__navbar-button-container">
+                            <Link to="/">
+                                <button className="single-recipe__navbar-button">
+                                    <SearchIcon className="single-recipe__navbar-icon" />
+                                </button>
+                            </Link>
+                            <Link to="/cookbook">
+                                <button className="single-recipe__navbar-button">
+                                    <BookIcon className="single-recipe__navbar-icon" />
+                                </button>
+                            </Link>
+                            </article>
+                    </div>
+               <div className="single-recipe__image-container">
+            <img className="single-recipe__image" src={recipe.image} alt={recipe.name} />
+            <div className="single-recipe__image-overlay"></div>
+          </div>
+            <p>{recipe.name}</p> 
+            </article>
           )}
         </section>
     );
