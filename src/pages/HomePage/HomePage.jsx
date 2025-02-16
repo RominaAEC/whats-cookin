28c3f2458ac1c334fb9c702860506ec375cdb779
Romina Esparza <romi.anahi01@outlook.com>
2025-02-15 19:24:15 -0500

    cleaned code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,37 @@
 import "./HomePage.scss";
+import { useState, useEffect } from "react";
 import PotIcon from "../../assets/icons/arcticons_rakuten-recipe.svg"
-import SearchByIngredients from "../../components/SearchByIngredients/SearchByIngredients";
 
 export default function HomePage() {
+  // Search form logic and functionality
+  const [searchState, setSearchState] = useState("default");
+
+  const handleFocus = (e) => {
+    setSearchState("active");
+  };
+
+  const handleBlur = (e) => {
+    setSearchState("default");
+  };
+
   return (
     <section className="home">
       <article className="home__container">
         <img className="home__image" src={PotIcon} alt="cooking pot icon" />
         <h1 className="home__title">Got ingredients? <br />Let's make something tasty!</h1>
-        <SearchByIngredients className="home__search" />
+        <form className="search-ingredients">
+          <input
+            type="text"
+            className={`search-ingredients__input ${searchState === "active" ? "search-ingredients__input--active" : ""
+              }`}
+            placeholder="Your ingredients..."
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+          <button type="submit" className="search-ingredients__button">
+            Find me a recipe
+          </button>
+        </form>
       </article>
 
     </section>
--- src/pages/RecipeSearchPage/RecipeSearchPage.jsx
+++ src/pages/RecipeSearchPage/RecipeSearchPage.jsx
@@ -1,30 +1,52 @@
 import "./RecipeSearchPage.scss";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import SearchByIngredients from "../../components/SearchByIngredients/SearchByIngredients";
 import SearchRecipeCard from "../../components/SearchRecipeCard/SearchRecipeCard";
 
 export default function RecipeSearchPage() {
   const [recipes, setRecipes] = useState([]); // State to store recipes data
 
   const getRecipes = async () => {
-    try{
-        const response = await axios.get('https://dummyjson.com/recipes')
-        console.log(response.data);
-        setRecipes(response.data.recipes);
-    } catch(error){
-        console.error("Error fetching recipes", error);
+    try {
+      const response = await axios.get('https://dummyjson.com/recipes')
+      console.log(response.data);
+      setRecipes(response.data.recipes);
+    } catch (error) {
+      console.error("Error fetching recipes", error);
     }
-}
+  }
 
-useEffect(() => {
+  useEffect(() => {
     getRecipes();
-}, []);
+  }, []);
+
+ // Search form logic and functionality
+  const [searchState, setSearchState] = useState("default");
+
+  const handleFocus = (e) => {
+    setSearchState("active");
+  };
+
+  const handleBlur = (e) => {
+    setSearchState("default");
+  };
 
   return (
-    <section className="recipe-search"> 
-      <SearchByIngredients className="recipe-search__search" />
-      <SearchRecipeCard recipes={recipes} basePath="/search-results"/>
+    <section className="recipe-search">
+      <form className="search-ingredients">
+        <input
+          type="text"
+          className={`search-ingredients__input ${searchState === "active" ? "search-ingredients__input--active" : ""
+            }`}
+          placeholder="Your ingredients..."
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+        <button type="submit" className="search-ingredients__button">
+          Find me a recipe
+        </button>
+      </form>
+      <SearchRecipeCard recipes={recipes} basePath="/search-results" />
     </section>
   )
 }
